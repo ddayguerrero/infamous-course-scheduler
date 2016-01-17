b63ebf0bfe7475dd43ab58e3b8675d386e7dab24
Darrel <ddayy.guerrero@hotmail.com>
2016-01-17 15:55:44 -0500

    Cleaned views.py; imported flask and render_template

--- app/views.py
+++ app/views.py
@@ -1,23 +1,7 @@
 from app import app
+from flask import render_template
 
 @app.route("/")
 @app.route("/home")
 def login():
-    data = [1,2,3]
-    return "Hello World!"#render_template('home.html')
-"""
-# returns the main Schedule html page
-@app.route("/Schedule/<username>")
-def Schedule(username):
-	return render_template('schedule.html',name=username)
-
-
-#returns list of classes in JSON format from sql
-@app.route("/classes")
-def classes():
-	my_people = {'Alice': 25,
-		     'Bob': 21,
-		     'Charlie': 20,
-		     'Doug': 28}
-	return jsonify(my_people);
-"""
+    return render_template('home.html')
