f78f68702100478c3e4cfb222a96e16403e6ee69
David <daviddube91@hotmail.com>
2016-01-18 14:41:07 -0500

    Made titles variable and completed the skeleton login page

--- app/views.py
+++ app/views.py
@@ -5,4 +5,9 @@ from app import app
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html',page="home")
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html',page="login")
