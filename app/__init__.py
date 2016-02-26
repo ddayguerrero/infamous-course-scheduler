5214b5695cf3e0e767686b7cc5b6fbd15f968313
Darrel <ddayy.guerrero@hotmail.com>
2016-02-26 11:08:14 -0500

    Add default route (login)

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 # Configurations
 app.config.from_object('config')
 
+@app.route('/')
+@app.route('/login')
+def home():
+    return render_template('auth/login.html', page="home")
+
 # Sample HTTP error handling
 @app.errorhandler(404)
 def not_found(error):
