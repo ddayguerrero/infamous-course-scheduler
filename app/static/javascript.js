a9a2a091198a743b5c71bdbbf26272702f6782f3
McGiver- <gkolampas@gmail.com>
2016-02-28 21:34:49 -0500

    added password registration front end validation

---
+++ app/static/javascript.js
@@ -0,0 +1,35 @@
+function validatePass(str){
+  if(!(/[0-9]/.test(str))){
+    alert("Password must contain atleast one number")
+    return false;
+  }
+  if(!(/[a-z]/.test(str))){
+    alert("Password must contain atleast one lowercase letter")
+    return false;
+  }
+  if(!(/[A-Z]/.test(str))){
+    alert("Password must contain atleast one uppercase letter")
+    return false;
+  }
+  if(str.length < 6 || str.length > 16){
+    alert("Password must contain a minimun of 6 character and a maximum of 16 characters")
+    return false;
+  }
+  if(!(/^\w+$/.test(str))){
+    alert("Password cannot contain special characters")
+    return false;
+  }
+  return true;
+}
+
+function checkPass(){
+  if(!validatePass(document.getElementById('password').value)){
+    alert("Unvalid password")
+  }
+}
+
+function confirmSame(){
+  if(document.getElementById('password').value != document.getElementById('confirmPass').value){
+    alert("The passwords are not the same");
+  }
+}
