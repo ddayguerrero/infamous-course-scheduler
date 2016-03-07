eb2475cdc0edcbe196c7e9eca06f57cabb4b51b4
Robert McAuley <mcauley.robert@live.com>
2016-03-07 15:02:38 -0500

    changed the table structure to match the registration form

--- app/module_authentication/models.py
+++ app/module_authentication/models.py
@@ -5,9 +5,11 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     user_Name = db.Column(db.String(30), unique = True)
     password = db.Column(db.String(20), index = True)
-    first_Name = db.Column(db.String(20), index = True)
-    last_Name = db.Column(db.String(20), index = True)
+    email = db.Column(db.String(50), index = True)
+    full_name = db.Column(db.String(50), index = True)
     courseRegister = db.relationship('courseRegistered', backref = "registered", lazy = "dynamic")
 
     def __repr__(self):
         return '<User %r>' % (self.user_Name)
+
+
