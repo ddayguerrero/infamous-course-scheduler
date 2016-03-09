91146a019ff0c1244cb359d7249f01b09f78fa43
Robert McAuley <mcauley.robert@live.com>
2016-03-09 16:39:26 -0500

    added validators to forms.py

--- app/module_authentication/forms.py
+++ app/module_authentication/forms.py
@@ -4,8 +4,11 @@ from flask.ext.wtf import Form
 from wtforms import BooleanField, TextField, PasswordField, validators
 
 class RegistrationForm(Form):
-    username = TextField('Username')
-    email = TextField('Email Address')
-    password = PasswordField('New Password')
+    username = TextField('Username', [validators.Length(min=4, max=25)])
+    email = TextField('Email Address', [validators.Length(min=6, max=35)])
+    password = PasswordField('New Password', [
+    	validators.Required(),
+    	validators.EqualTo('confirm', message='Passwords must match')
+    ])
     confirm = PasswordField('Repeat Password')
-    accept_tos = BooleanField('I accept the TOS')
+    accept_tos = BooleanField('I accept the TOS', [validators.Required()])
