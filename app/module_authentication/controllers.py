9c15850b388f3e925d5d58d8d0e8fe9686b1dc13
skellyboy38 <mcauley.robert@live.com>
2016-03-09 11:37:52 -0500

    added form template

--- app/module_authentication/controllers.py
+++ app/module_authentication/controllers.py
@@ -8,10 +8,10 @@ from werkzeug import check_password_hash, generate_password_hash
 from app import db
 
 #Import module forms
-#from app.module_authentication.forms import LoginForm
+from app.module_authentication.forms import RegistrationForm
 
 #Import module models
-#from app.module_authentication.models import User
+from app.module_authentication.models import User
 
 #Define the blueprint: 'auth'
 mod_auth = Blueprint('auth',__name__)
@@ -22,11 +22,20 @@ def home():
     return render_template('auth/login.html', page="home")
 
 
-@mod_auth.route('/registration/')
+@mod_auth.route('/registration/', methods=['GET', 'POST'])
 def register():
-    return render_template('auth/registration.html', page="register")
+	form = RegistrationForm()
+	if request.method == 'POST' and form.validate_on_submit():
+		user = User(form.username.data, form.email.data, form.password.data)
+		db.session.add(user)
+		db.session.commit()
+		session['user_id'] = user.id
+		flash('Thanks for registering')
+		return redirect(url_for('auth.login'))
+	return render_template('auth/registration.html', page="register", form=form)
 
-
-@mod_auth.route('/home')
+@mod_auth.route('/home/')
 def studentHome():
     return render_template('auth/home.html', page="portal")
+
+
--- app/module_authentication/forms.py
+++ app/module_authentication/forms.py
@@ -1,1 +1,11 @@
+
+from flask.ext.wtf import Form
 # Login and Register forms should be here
+from wtforms import BooleanField, TextField, PasswordField, validators
+
+class RegistrationForm(Form):
+    username = TextField('Username')
+    email = TextField('Email Address')
+    password = PasswordField('New Password')
+    confirm = PasswordField('Repeat Password')
+    accept_tos = BooleanField('I accept the TOS')
