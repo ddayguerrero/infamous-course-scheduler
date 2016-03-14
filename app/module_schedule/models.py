1470cbb45a383a3077398496f796656ad93a3592
skellyboy38 <mcauley.robert@live.com>
2016-03-14 15:44:08 -0400

    added models for the schedule module

--- app/module_schedule/models.py
+++ app/module_schedule/models.py
@@ -3,8 +3,46 @@ from app import db
 class Student(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     full_name = db.Column(db.String(50), index = True)
-    courseRegister = db.relationship('courseRegistered', backref = "registered", lazy = "dynamic")
+    course_register = db.Column(db.Integer, db.ForeignKey('courseRegistered.id'))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    sequence_id = db.Column(db.Integer, db.ForeignKey('sequence.id'))
+    course_completed_id = db.Column(db.Integer, db.ForeignKey('courseCompleted.id'))
 
     def __repr__(self):
         return '<User %r>' % (self.full_name)
+
+
+class CourseCompleted(db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+	course_id = db.Column(db.Integer, db.ForeignKey('course.id'))
+
+	def __repr__(self):
+        return '<CourseCompleted %r>' % (self.id)
+
+
+class Semester(db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+	course_id = db.Column(db.Integer, db.ForeignKey('course.id'))
+
+	def __repr__(self):
+		return '<Term %r>' % (self.id)
+
+class Sequence(db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+	course_id = db.Column(db.Integer, db.ForeignKey('course.id'))
+
+	def __repr__(self):
+        return '<Sequence %r>' % (self.id)
+
+class Prerequisites(db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+
+	def __repr__(self):
+        return '<Prerequisite %r>' % (self.id)
+
+class CourseRegistered(db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+	course_id = db.Column(db.Integer, db.ForeignKey('course.id'))
+
+	def __repr__(self):
+		return '<CourseRegistered %r>' % (self.id)
