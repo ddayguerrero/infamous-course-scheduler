36535470a989c6c68444cc9f9f0f106d32f64335
Darrel <ddayy.guerrero@hotmail.com>
2016-03-14 15:54:14 -0400

    Add course, section, lab, and tutorial

--- app/module_schedule/models.py
+++ app/module_schedule/models.py
@@ -1,12 +1,13 @@
 from app import db
 
+
 class Student(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    full_name = db.Column(db.String(50), index = True)
-    course_register = db.Column(db.Integer, db.ForeignKey('courseRegistered.id'))
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(50), index=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     sequence_id = db.Column(db.Integer, db.ForeignKey('sequence.id'))
     course_completed_id = db.Column(db.Integer, db.ForeignKey('courseCompleted.id'))
+    course_register = db.Column(db.Integer, db.ForeignKey('courseRegistered.id'))
 
     def __repr__(self):
         return '<User %r>' % (self.full_name)
@@ -27,6 +28,7 @@ class Semester(db.Model):
 	def __repr__(self):
 		return '<Term %r>' % (self.id)
 
+
 class Sequence(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
 	course_id = db.Column(db.Integer, db.ForeignKey('course.id'))
@@ -34,11 +36,13 @@ class Sequence(db.Model):
 	def __repr__(self):
         return '<Sequence %r>' % (self.id)
 
+
 class Prerequisites(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
 
 	def __repr__(self):
         return '<Prerequisite %r>' % (self.id)
+
 
 class CourseRegistered(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
@@ -46,3 +50,38 @@ class CourseRegistered(db.Model):
 
 	def __repr__(self):
 		return '<CourseRegistered %r>' % (self.id)
+
+
+class Course(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    code = db.Column(db.String(4))
+    credits = db.Column(db.Double(4))
+    number = db.Column(db.String(3))
+    name = db.Column(db.String(50))
+
+
+class Lab(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    course_id = db.Column(db.Integer, db.ForeignKey('course.id'))
+    start_time = db.Colum(db.DateTime, default=db.func.now())
+    end_time = db.Colum(db.DateTime, default=db.func.now())
+    days = db.Column(db.String(25))
+    
+
+class Section(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    course_id = db.Column(db.Integer, db.ForeignKey('course.id'))
+    code = db.Column(db.String(3))
+    start_time = db.Colum(db.DateTime, default=db.func.now())
+    end_time = db.Colum(db.DateTime, default=db.func.now())
+    professor = db.Column(db.Integer(50))
+    days = db.Column(db.String(25))
+    semester = db.Column(db.Integer, db.ForeignKey('semester.id'))
+    
+
+class Tutorial(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    course_id = db.Column(db.Integer, db.ForeignKey('course.id'))
+    start_time = db.Colum(db.DateTime, default=db.func.now())
+    end_time = db.Colum(db.DateTime, default=db.func.now())
+    days = db.Column(db.String(25))
