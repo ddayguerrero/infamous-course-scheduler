3f4685eb57433311b2a19f8b118537ae2d55f2bb
Robert McAuley <mcauley.robert@live.com>
2016-03-13 21:03:17 -0400

    changed model for user and added a student model

--- app/module_authentication/models.py
+++ app/module_authentication/models.py
@@ -6,10 +6,18 @@ class User(db.Model):
     user_Name = db.Column(db.String(30), unique = True)
     password = db.Column(db.String(20), index = True)
     email = db.Column(db.String(50), index = True)
-    full_name = db.Column(db.String(50), index = True)
-    courseRegister = db.relationship('courseRegistered', backref = "registered", lazy = "dynamic")
 
     def __repr__(self):
         return '<User %r>' % (self.user_Name)
+
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    full_name = db.Column(db.String(50), index = True)
+    courseRegister = db.relationship('courseRegistered', backref = "registered", lazy = "dynamic")
+    user_id = db.relationship('user', backref = "User")
+
+    def __repr__(self):
+        return '<User %r>' % (self.full_name)
 
 
