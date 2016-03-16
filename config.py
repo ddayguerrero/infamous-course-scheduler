afb119dd7e0ffe97d09ceaba44d82b60ef016cb9
skellyboy38 <mcauley.robert@live.com>
2016-03-16 12:30:13 -0400

    modified init and config for db creation

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 # Define the database - we are working with
 # SQLite for this example
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(BASE_DIR, 'db_repository')
 DATABASE_CONNECT_OPTIONS = {}
 
 #Turn off database object tracking (overhead)
