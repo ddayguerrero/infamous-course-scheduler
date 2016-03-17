21142efe2d1aa7020d6f89f551671b5b17e5c8c0
Progaine <alidouch@hotmail.com>
2016-03-16 21:21:20 -0400

    Added email validation

--- app/module_authentication/forms.py
+++ app/module_authentication/forms.py
@@ -5,7 +5,7 @@ from wtforms import BooleanField, TextField, PasswordField, validators
 
 class RegistrationForm(Form):
     username = TextField('Username', [validators.Length(min=4, max=25)])
-    email = TextField('Email Address', [validators.Length(min=6, max=35)])
+    email = TextField('Email Address', [validators.Email(), validators.Length(min=6, max=35)])
     password = PasswordField('New Password', [
     	validators.Required(),
     	validators.EqualTo('confirm', message='Passwords must match')
