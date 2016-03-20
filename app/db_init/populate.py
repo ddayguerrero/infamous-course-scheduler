d82098cf84568a3d0106b4a83ec0fe91a72bd274
Darrel <ddayy.guerrero@hotmail.com>
2016-03-20 16:10:19 -0400

    Parse and insert lectures data

--- app/db_init/populate.py
+++ app/db_init/populate.py
@@ -1,48 +1,23 @@
 import csv
-from app import db
 import re
-# Import the tables you need 
-def parseAllFiles():
-	# Call all helper functions
-	parseCourseList()
-	parseSequences()
-	
+
 
 def parseLectures():
-	# Fix paths - this won't work
-    with open("lectures.csv") as csvfile:
+    # Fix paths - this won't work
+    with open("data/lectures.csv") as csvfile:
         reader = csv.DictReader(csvfile)
-		for row in reader:
-			days = filter(None,re.split(r'[-]+',row['days']))
+        for row in reader:
+            days = filter(None, re.split(r'[-]+', row['days']))
+            semester_id = getSemesterId(row['semester'])
+            print semester_id
+            # db.session.add(Lecture(row['instructor'], row['course_id'], semester_id))
 
+def getSemesterId(sem):
+    return {
+        'Fall': 0,
+        'Winter': 1,
+        'Summer': 2
+    }.get(sem, -1)
+            
 
-# Combines these two into one function
-def parseTutorials():
-def parseLabs():
-
-def parseCourseList():
-	# Fix paths - this won't work
-	with open('courses.csv') as csv_file:
-		reader = csv.DictReader(csv_file)
-		for row in reader:
-			db.session.add(Course(row['program'], row['number'], row['credits'], row['name'])
-		db.session.commit()
-		
-def parseSequences():
-	# Fix paths - this won't work
-	files = ['sequenceAvionics.csv', 'sequenceGames.csv', 'sequenceGeneral.csv', 'sequenceWeb.csv']
-	for file in files:
-		with open(file) as csv_file:
-			reader = csv.DictReader(csv_file)
-				# from the imported table, call insert().values(...)
-				# ex: sequences.insert().values(option="Web")
-
-def parsePrerequisites():
-	# Fix paths - this won't work
-	with open('course_prereqs.csv') as csv_file:
-			reader = csv.DictReader(csv_file)
-
-def parseElectives(files):
-	
-def parseSequenceElectives():
-	#Sequences need a foreign key associated with sequence_electives table
+parseLectures()
