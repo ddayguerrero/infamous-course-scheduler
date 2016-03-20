d336e3d1c56aa959f881e535b3122de8c3e47647
Robert McAuley <mcauley.robert@live.com>
2016-03-20 15:18:30 -0400

    removed hash imports

--- app/module_authentication/controllers.py
+++ app/module_authentication/controllers.py
@@ -1,9 +1,6 @@
 # Import flask dependencies
 from flask import Blueprint, request, render_template, flash, g, session, redirect, url_for
 
-# Import password / encryption helper tools
-from werkzeug import check_password_hash, generate_password_hash
-
 # Import module forms
 from app.module_authentication.forms import RegistrationForm, LoginForm
 
