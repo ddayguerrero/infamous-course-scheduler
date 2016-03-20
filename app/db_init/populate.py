0703d79bdf896437cbeb240717cffb84dc561df5
Darrel <ddayy.guerrero@hotmail.com>
2016-03-20 17:06:31 -0400

    Parse program sequences

--- app/db_init/populate.py
+++ app/db_init/populate.py
@@ -6,19 +6,19 @@ def parseCourses():
     with open("data/course.csv") as csvFile:
         reader = csv.DictReader(csvFile)
         for row in reader:
-            #db.session.add(Course(row['program'], row['credits'], row['number'], row['name']))
-            #db.session.commit()
+            # db.session.add(Course(row['program'], row['credits'], row['number'], row['name']))
+            # db.session.commit()
 
 
-def parseLectures():
-    with open("data/lectures.csv") as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            days = getDays(row['days'])
-            semester_id = getSemesterId(row['semester'])
-            print semester_id
-            #db.session.add(Lecture(row['instructor'], row['course_id'], semester_id))
-            #db.session.commit()
+# def parseLectures():
+#     with open("data/lectures.csv") as csvfile:
+#         reader = csv.DictReader(csvfile)
+#         for row in reader:
+#             days = getDays(row['days'])
+#             semester_id = getSemesterId(row['semester'])
+#             print semester_id
+#             #db.session.add(Lecture(row['instructor'], row['course_id'], semester_id))
+#             #db.session.commit()
 
 
 def parseLabs():
@@ -37,6 +37,25 @@ def parseTutorials():
             db.session.add(Lecture(row['lecture_id'], row['section_code'], row['day'], row['end_time'], days))
 
 
+def parseSequences():
+    files = ['sequenceAvionics.csv', 'sequenceGames.csv', 'sequenceGeneral.csv', 'sequenceWeb.csv']
+    for file in files:
+        with open('data/SequenceRequired'+file) as csv_file:
+            reader = csv.DictReader(csv_file)
+            opt = file[8, file.index('.')]
+            sequences.insert().values(option=opt, course_id=row['course_id'])
+
+
+def parseSequences():
+    # Fix paths - this won't work
+    files = ['sequenceAvionics.csv', 'sequenceGames.csv', 'sequenceGeneral.csv', 'sequenceWeb.csv']
+    for file in files:
+        with open(file) as csv_file:
+            reader = csv.DictReader(csv_file)
+            opt = file[8, file.index('.')]
+            sequences.insert().values(option=opt, course_id=row['course_id'])
+
+
 def getDays(days):
     return filter(None, re.split(r'[-]+', days))
 
@@ -48,4 +67,4 @@ def getSemesterId(sem):
         'Summer': 2
     }.get(sem, -1)
 
-parseLectures()
+parseSequences()
