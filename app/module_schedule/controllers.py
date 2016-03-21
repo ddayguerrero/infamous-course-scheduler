5fac1d86923132812d043fb947b5bd3e11e32f73
Darrel <ddayy.guerrero@hotmail.com>
2016-03-21 19:00:02 -0400

    WIP: Design RESTful API

--- app/module_schedule/controllers.py
+++ app/module_schedule/controllers.py
@@ -1,24 +1,84 @@
-#Import flask dependencies
-from flask import Blueprint, request, render_template, flash, g, session, redirect, url_for
+from flask import Flask, Blueprint, jsonify, abort, request
 
-#Import the database object from the main app module
-from app import db
+app = Flask(__name__)
+mod_schedule = Blueprint('schedule', __name__)
 
-#Import module forms
+# Test Data - to be removed later
+# JSON stores values in key-value pairs
+courses = [
+    {
+        'id': 1,
+        'program': 'ENGR',
+        'number': 201,
+        'credits': 1.5,
+        'name': "Professional Practice and Responsibility"
+    },
+    {
+        'id': 2,
+        'program': 'ENGR',
+        'number': 202,
+        'credits': 3,
+        'name': 'Sustainable Development and Environmental Stewardship'
+    }
+]
+
+schedules = [
+    {
+        'id': 1,
+        'name': "Lerrad"
+    },
+    {
+        'id': 2,
+        'name': "Neomis"
+    },
+    {
+        'id': 3,
+        'name': "Divad"
+    }
+]
 
 
-#Define the blueprint
-mod_schedule = Blueprint('schedule',__name__)
+# RESTful resources must be defined as 'nouns' not verbs
+# e.g. /courses istead of /addcourses
+#
+# GET request example
+@mod_schedule.route('/courses', methods=['GET'])
+def get_courses():
+    # logic goes here
+    return jsonify({'tasks': courses})
 
-#Define functions that will handle routes
-#@mod_schedule.route('/')
-#def home():
-
-
-#@mod_schedule('', methods=['GET', 'POST'])
-#def register():
-	
-
-    
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
+# Say you want to retrieve a specific course (e.g. based on id)
+#
+# GET request example
+@mod_schedule.route('/courses/<int:id>', methods=['GET'])
+def get_course(id):
+    course = [course for course in courses if course['id'] == id]
+
+    # no existing course
+    if len(course) == 0:
+        abort(404)
+
+    return jsonify({'course': course[0]})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+# Say you want to create a specific resource (e.g. schedule)
+#
+# POST request example
+@mod_schedule.route('/schedules', methods=['POST'])
+def generate_schedule():
+    if not request.json or 'name' not in request.json:
+        abort(400)
+    schedule = {
+        'id': schedules[-1]['id'] + 1,  # schedule is equal to last schedule id + 1
+        'name': request.json['name'],
+        'done': False
+    }
+    schedules.append(schedule)
+    return jsonify({'schedule': schedule}), 201
