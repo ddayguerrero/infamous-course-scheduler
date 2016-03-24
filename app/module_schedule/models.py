63376a303d8d5ebdbdc6ff6c8e550554a15b0977
Simz <bitugos@gmail.com>
2016-03-24 03:39:00 -0400

    Add constructors

--- app/module_schedule/models.py
+++ app/module_schedule/models.py
@@ -1,7 +1,6 @@
 from app import db
 from app.abstract_models import Abstract_Base, Abstract_ClassType, Abstract_Course
 from app.module_authentication.models import User
-from sqlalchemy.orm import relationship
 
 
 # Concrete Models
@@ -24,17 +23,14 @@ class Student(Abstract_Base):
 
 class Course(Abstract_Course):
     __tablename__ = 'courses'
-    type = db.Column(db.String(30))
-    requisite_for = db.Column(db.Integer, db.ForeignKey('mappings.id'))
-    lectures = relationship("Lecture")
+    requisite = db.Column(db.Integer, db.ForeignKey('mappings.id'))
+    lectures = db.relationship("Lecture")
 
-    def __init__(self, program=None, credits=None, number=None, name=None):
-        self.program = program
-        self.credits = credits
-        self.number = number
-        self.name = name
+    def __init__(self, program=None, credits=None, number=None, name=None, requisite=None):
+        super(type(self), self).__init__(program=None, number=None, credits=None, name=None)
+        self.requisite = requisite
 
-    def __repr__(self):
+    def__repr__(self):
         return '<Course %r>' % (self.name)
 
 
@@ -43,30 +39,22 @@ class Lab(Abstract_ClassType):
     lecture_id = db.Column(db.Integer, db.ForeignKey('lectures.id'))
 
     def __init__(self, lecture_id=None, section_code=None, start_time=None, end_time=None, day_one=None):
+        super(type(self), self).__init__(code=None, start_time=None,end_time=None, day_one=None, day_two=None)
         self.lecture_id = lecture_id
-        self.section_code = section_code
-        self.start_time = start_time
-        self.end_time = end_time
-        self.day_one = day_one
 
     def __repr__(self):
         return '<Lab %r>' % (self.code)
-        
+
 
 class Tutorial(Abstract_ClassType):
     __tablename__ = 'tutorials'
     lecture_id = db.Column(db.Integer, db.ForeignKey('lectures.id'))
 
-    def __init__(self, lecture_id=None, section_code=None, start_time=None, end_time=None, day_one=None, day_two=None):
+    def __init__(self, lecture_id=None, section_code=None, start_time=None, end_time=None, day_one=None):
+        super(type(self), self).__init__(code=None, start_time=None,end_time=None, day_one=None, day_two=None)
         self.lecture_id = lecture_id
-        self.section_code = section_code
-        self.start_time = start_time
-        self.end_time = end_time
-        self.day_one = day_one
-        self.day_two = day_two
-
     def __repr__(self):
-        return '<Tutorial %r>' % (self.section_id)
+        return '<Tutorial %r>' % (self.code)
 
 
 class Lecture(Abstract_ClassType):
@@ -74,20 +62,18 @@ class Lecture(Abstract_ClassType):
     instructor = db.Column(db.String(50))
     course_id = db.Column(db.Integer, db.ForeignKey('courses.id'))
     semester_id = db.Column(db.Integer, db.ForeignKey('semesters.id'))
-    tutorial = relationship("Tutorial")
-    lab = relationship("Lab")
 
-    def __init__(self, instructor=None, course_id=None, semester_id=None, start_time=None, end_time=None, day_one=None, day_two=None):
+    tutorial = db.relationship("Tutorial")
+    lab = db.relationship("Lab")
+
+    def __init__(self, instructor=None, course_id=None, semester_id=None, code=None, start_time=None, end_time=None, day_one=None, day_two=None):
+        super(type(self), self).__init__(code=None, start_time=None, end_time=None, day_one=None, day_two=None)
         self.instructor = instructor
         self.course_id = course_id
         self.semester_id = semester_id
-        self.start_time = start_time
-        self.end_time = end_time
-        self.day_one = day_one
-        self.day_two = day_two
 
     def __repr__(self):
-        return '<Lecture %r>' % (self.section_id)
+        return '<Lecture %r>' % (self.code)
 
 
 class AcademicRecord(Abstract_Base):
@@ -102,7 +88,7 @@ class AcademicRecord(Abstract_Base):
         self.course_status = course_status
 
     def __repr__(self):
-        return '<User %r>' % (self.id)
+        return '<AcademicRecord %r>' % (self.id)
 
 
 class Semester(Abstract_Base):
@@ -115,7 +101,7 @@ class Semester(Abstract_Base):
         self.course_id = course_id
 
     def __repr__(self):
-        return '<User %r>' % (self.id)
+        return '<Semester %r>' % (self.id)
 
 
 class Sequence(Abstract_Base):
@@ -128,7 +114,7 @@ class Sequence(Abstract_Base):
         self.course_id = course_id
 
     def __repr__(self):
-        return '<User %r>' % (self.id)
+        return '<Sequence %r>' % (self.id)
 
 
 class Mapping(Abstract_Base):
@@ -143,7 +129,7 @@ class Mapping(Abstract_Base):
         self.course_id = course_id
 
     def __repr__(self):
-        return '<User %r>' % (self.id)
+        return '<Mapping %r>' % (self.id)
 
 
 class Elective(Abstract_Base):
@@ -156,5 +142,5 @@ class Elective(Abstract_Base):
         self.course_id = course_id
 
     def __repr__(self):
-        return '<User %r>' % (self.id)
+        return '<Elective %r>' % (self.id)
 
