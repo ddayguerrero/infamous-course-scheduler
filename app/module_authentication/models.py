3e1ddafa2e5818a2fe7e9163c136a78c59995615
Simz <bitugos@gmail.com>
2016-03-24 03:40:54 -0400

    Specify One-to-One between User and Student

--- app/module_authentication/models.py
+++ app/module_authentication/models.py
@@ -7,11 +7,14 @@ class User(Abstract_Base):
     username = db.Column(db.String(30), unique = True)
     password = db.Column(db.String(20), index = True)
     email = db.Column(db.String(50), index = True)
-    	# foreign key to Student
-    def __init__(self, username=None, password=None, email=None):
+    student_id = db.Column(db.Integer, db.ForeignKey('students.id'))
+    student = db.relationship('students', backref=backref('users', uselist=False))
+    
+    def __init__(self, username=None, password=None, email=None, student_id=None):
     	self.username = username
     	self.password = password
     	self.email = email
+    	self.student_id = student_id
 
     def __repr__(self):
         return '<User %r>' % (self.username)
