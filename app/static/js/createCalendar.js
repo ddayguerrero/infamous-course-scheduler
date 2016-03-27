b60f970550eb15ae0e4091b3a2b7fc54f04bc86c
Harrison <harrison.a101@hotmail.com>
2016-03-27 14:01:41 -0400

    added javascript code to file which dynamically generates a calander

--- app/static/js/createCalendar.js
+++ app/static/js/createCalendar.js
@@ -3,37 +3,41 @@ function tableCreate(){
  tbl  = document.createElement('table');
  tbl.className = "table table-striped";
 
-   var header = tbl.createTHead();
-   var row = header.insertRow(0);
-   var cell = row.insertCell(0);
-   var cell = row.insertCell(1);
-   cell.innerHTML = "Monday";
-   var cell = row.insertCell(2);
-   cell.innerHTML = "Tuesday";
-   var cell = row.insertCell(3);
-   cell.innerHTML = "Wednesday";
-   var cell = row.insertCell(4);
-   cell.innerHTML = "Thursday";
-   var cell = row.insertCell(5);
-   cell.innerHTML = "Friday";
-   var cell = row.insertCell(6);
-   cell.innerHTML = "Saturday";
-   var cell = row.insertCell(7);
-   cell.innerHTML = "Sunday";
+ var header = tbl.createTHead();
+ var row = header.insertRow(0);
+ var cell = row.insertCell(0);
+ var cell = row.insertCell(1);
+ cell.innerHTML = "Monday";
+ var cell = row.insertCell(2);
+ cell.innerHTML = "Tuesday";
+ var cell = row.insertCell(3);
+ cell.innerHTML = "Wednesday";
+ var cell = row.insertCell(4);
+ cell.innerHTML = "Thursday";
+ var cell = row.insertCell(5);
+ cell.innerHTML = "Friday";
+ var cell = row.insertCell(6);
+ cell.innerHTML = "Saturday";
+ var cell = row.insertCell(7);
+ cell.innerHTML = "Sunday";
  
 
  for(var i = 0; i < 52; i++){
   var tr = tbl.insertRow();
   for(var j = 0; j < 8; j++){
-      var td = tr.insertCell();
+    var td = tr.insertCell();
     if(j==0){
       var hour = 8+Math.floor(i/4);
-      td.appendChild(document.createTextNode(hour+":00"));
+      var min = (i%4)*15;
+      if(min!=0)
+        td.appendChild(document.createTextNode(hour+":"+min));
+      else
+        td.appendChild(document.createTextNode(hour+":00")); //min would only be 0 instead of 00
       td.style.border = '1px solid black';  
     } 
-      td.appendChild(document.createTextNode(""));
-      td.style.border = '1px solid black';  
-      
+    td.appendChild(document.createTextNode(""));
+    td.style.border = '1px solid black';  
+
   }
 }
 parent.appendChild(tbl);
@@ -43,28 +47,28 @@ parent.appendChild(tbl);
 // HTTP Requests for searching for courses
 //////////
 $( document ).ready(function() {
-    console.log("ready!");
-$('#addCourse').click(function(){
+  tableCreate();
+  $('#addCourse').click(function(){
     var inputCourse = $('input[name="courseSearch"]').val()
     $.ajax({
-        url: '/courses',
-        data: {
-            course: inputCourse
-        },
-        error: function(error) {
-          console.log(error)
-        },
-        success: function(data) {
-            console.log(data)
-            console.log("success")
-            $("#courseIdNum").html(data.id)
-            $("#courseName").html(data.name)
-            $("#startTime").html(data.startTime)
-            $("#endTime").html(data.endTime)
-            $("#instructor").html(data.instructor)
-        },
-        type: 'POST'
+      url: '/courses',
+      data: {
+        course: inputCourse
+      },
+      error: function(error) {
+        console.log(error)
+      },
+      success: function(data) {
+        console.log(data)
+        console.log("success")
+        $("#courseIdNum").html(data.id)
+        $("#courseName").html(data.name)
+        $("#startTime").html(data.startTime)
+        $("#endTime").html(data.endTime)
+        $("#instructor").html(data.instructor)
+      },
+      type: 'POST'
     });
-});
- 
+  });
+
 }); 
